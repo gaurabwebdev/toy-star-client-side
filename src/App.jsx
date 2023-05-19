ef71272c43f2e9c0a2d3e4fefcae90c6af0bcfc4
gaurabwebdev <gaurabbose40@gmail.com>
2023-05-19 19:21:32 +0600

    installed react router and set up basic of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "./Shared/Header/Header";
+import Footer from "./Shared/Footer/Footer";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <p className="text-3xl text-red-600">Khelaghor Client Site</p>
+      <Footer />
     </div>
   );
 };
