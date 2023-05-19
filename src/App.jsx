5a5fecdae5ed9e85512787c7d44bd90895d5ca38
gaurabwebdev <gaurabbose40@gmail.com>
2023-05-19 14:12:07 +0600

    first client site setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <p className="text-3xl text-red-600">Khelaghor Client Site</p>
+    </div>
+  );
+};
+
+export default App;
